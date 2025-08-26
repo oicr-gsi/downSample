f4b8f00601c64644bb48c5cde37d4908e8f47a7c
anallan <anallan@oicr.on.ca>
2025-08-26 16:55:59 -0400

    modified outputs

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -7,4 +7,4 @@ cd $1
 
 ls | sort
 
-find . -name "*.metrics" -not -name "neat_5x_EX_hg19_picard.downsample.metrics" -xtype f -exec sh -c "cat {} | md5sum" \;
+find . -name "*.metrics" -not -name "neat_5x_EX_hg19_picard.downsample.metrics" -type f -exec cat {} \;
